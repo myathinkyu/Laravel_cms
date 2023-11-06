038ddeeea13cc5b3c23aa7daca248c365482c02d
myathinkyu <myathinkyu.pp@gmail.com>
2023-11-06 17:35:39 +0630

    1-19

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//use small project
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//use big project
+Route::get('/post/{username}/{password}', 'App\Http\Controllers\PostsController@privacy');
+
+// Route::get('/home', function () {
+//     return "This is Homepage."; 
+// });
+
+// Route::get('/about', function () {
+//     return "This is About page."; 
+// });
+
+Route::get('/contact', 'App\Http\Controllers\PostsController@index');
+
+// Route::get('/post/{id}/{title}', function($id, $title) {
+//     return "The post id is " . $id . " and the post title is " . $title; 
+// });
+
+// Route::get('/post/example/test/title/personal/hey', function () {
+//     return "The post has long URL"; 
+// });
+
+// Route::get('/post/example/test/title/personal/hey', array('as'=>'localhost/laravel_cms/public/', function () {
+//     $url = route('localhost/laravel_cms/public/');
+//     return "The url is " . $url; 
+// }));
+
+
+
+
+
